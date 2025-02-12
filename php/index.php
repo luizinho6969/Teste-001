c3ebf3f1411ecc64f3e5bf5b31ff68b931870e71
luizinho6969 <louiskk49@gmail.com>
2025-02-11 22:04:05 -0300

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá mundo!</title>
+</head>
+<body>
+    <?php
+        echo "Olá mundo!"
+    ?>
+</body>
+</html>
